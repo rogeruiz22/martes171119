a2dfb4e85e599e5002c33db044540d4bc64d4944
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-18 10:33:23 -0400

    Email enviado automaticamente al equipo desde salesforce con Github Actions y Test de Selenium

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -11,7 +11,7 @@ public class AppTest
     @Test
     public void prueba() throws Exception {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://login.salesforce.com");
+        driver.get("https://teamorg2-dev-ed.lightning.force.com/lightning/o/Contact/list?filterName=Recent");
 
         WebElement username = driver.findElement(By.xpath("//input[@id='username']"));
         WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
@@ -21,18 +21,41 @@ public class AppTest
         password.sendKeys("incubator2022");
         loginButton.click();
 
-        Thread.sleep(20000);
+        Thread.sleep(18000);
 
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Home | Salesforce");
-
-        WebElement appLauncher = driver.findElement(By.xpath("//div[@class='navexSetupNav']//div//div/button"));
-        appLauncher.click();
+        WebElement selectAllContactsCheckbox = driver.findElement(By.xpath("(//span[@class='slds-checkbox--faux'])[1]"));
+        selectAllContactsCheckbox.click();
 
         Thread.sleep(3000);
 
-        WebElement appLauncherSearchBox = driver.findElement(By.xpath("//th[@class='selectionColumnHeader slds-has-focus']//span[@class='slds-checkbox--faux']"));
-        appLauncherSearchBox.sendKeys("Contacts" + Keys.RETURN);
+        WebElement sendListEmailButton = driver.findElement(By.xpath("//div[contains(text(), 'Send List Email')]"));
+        sendListEmailButton.click();
+
+        Thread.sleep(3000);
+
+        WebElement subjectField = driver.findElement(By.xpath("//input[@placeholder='Enter Subject...']"));
+        subjectField.sendKeys("Incubator Team - Github Actions funciona !");
+
+        WebElement emailBodyIframe = driver.findElement(By.xpath("//div[@class='content iframe-parent']//iframe"));
+        driver.switchTo().frame(emailBodyIframe);
+
+        Thread.sleep(3000);
+
+        WebElement emailBody = driver.findElement(By.xpath("//div[@id='cke_editor']//iframe"));
+        driver.switchTo().frame(emailBody);
+
+        Thread.sleep(10000);
+
+        WebElement emailBodyTextArea = driver.findElement(By.cssSelector("body"));
+        emailBodyTextArea.sendKeys("Este email fue enviado automaticamente usando workflows en Github Actions con Selenium test. Se ejecuto al hacerle push a la rama principal");
+
+        Thread.sleep(10000);
+
+        driver.switchTo().parentFrame();
+        driver.switchTo().parentFrame();
+
+        WebElement sendButton = driver.findElement(By.xpath("//button[text()='Send']"));
+        sendButton.click();
 
         Thread.sleep(3000);
 
