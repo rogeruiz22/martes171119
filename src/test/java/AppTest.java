3a941e246c6aa121dd16452b8118abcb12a80e4e
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-18 17:11:33 -0400

    Refactoring test time 8

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,7 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AppTest
@@ -27,17 +25,15 @@ public class AppTest
         WebElement selectRogerContact = driver.findElement(By.xpath("(//a[text()='Roger Ruiz']/ancestor::tr//td/following-sibling::td[@class='slds-cell-edit cellContainer'])[1]"));
         selectRogerContact.click();
 
-//         Thread.sleep(3000);
-
         WebElement sendListEmailButton = driver.findElement(By.xpath("//div[contains(text(), 'Send List Email')]"));
         sendListEmailButton.click();
 
-        Thread.sleep(5000);
+        Thread.sleep(4000);
 
         WebElement subjectField = driver.findElement(By.xpath("//input[@placeholder='Enter Subject...']"));
         subjectField.sendKeys("Incubator Team - Github Actions funciona !!! :)");
         
-        Thread.sleep(4000);
+        Thread.sleep(3000);
 
         WebElement emailBodyIframe = driver.findElement(By.xpath("//div[@class='content iframe-parent']//iframe"));
         driver.switchTo().frame(emailBodyIframe);
@@ -47,22 +43,22 @@ public class AppTest
         WebElement emailBody = driver.findElement(By.xpath("//div[@id='cke_editor']//iframe"));
         driver.switchTo().frame(emailBody);
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         WebElement emailBodyTextArea = driver.findElement(By.cssSelector("body"));
         emailBodyTextArea.sendKeys("Este email fue enviado automaticamente usando workflows en " +
                 "Github Actions con Selenium test. Se ejecuto y se ejecutara al hacerle push a la rama principal. " +
                 "Github Repo: https://github.com/rogeruiz22/martes171119.git");
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         driver.switchTo().parentFrame();
-        driver.switchTo().parentFrame();
+//        driver.switchTo().parentFrame();
 
         WebElement sendButton = driver.findElement(By.xpath("//button[text()='Send']"));
         sendButton.click();
 
-        Thread.sleep(3000);
+        Thread.sleep(1000);
 
         driver.quit();
     }
